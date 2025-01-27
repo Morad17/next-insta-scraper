058d34b8d8a6bcc9413889c36b8577fc5065bf6a
Morad17 <morad123@hotmail.co.uk>
2025-01-27 20:50:50 +0000

    testing out next

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,29 @@
 import Image from "next/image";
+import { useState } from "react";
 
 export default async function Home() {
+
+  const [searchTerm, setSearchTerm] = useState('')
+
+  const url = process.env.HASHTAG_URL + searchTerm;
+  const options = {
+    method: 'GET',
+    headers: {
+      'x-rapidapi-key': process.env.RAPIDAPI_KEY,
+      'x-rapidapi-host': process.env.RAPIDAPI_HOST
+    }
+  };
+
   const data = await fetch("") 
 
 
   return (
     <div className="">
         <h2>Hello world</h2>
+        <form action="">
+          
+        </form>
+        <input type="text" onChange={()=> setSearchTerm()} />
     </div>
   );
 }
